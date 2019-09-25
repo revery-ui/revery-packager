322a50ecee56bf8d3bae92b29667b0f777f05bd5
bryphe <bryphe@outrunlabs.com>
2019-09-25 14:01:19 -0700

    Update package-windows

--- src/package-windows.js
+++ src/package-windows.js
@@ -1,8 +1,21 @@
+const fs = require("fs-extra");
+const path = require("path");
+
 const util = require("./util");
 const esy = require("./esy");
 
 module.exports = (config) => {
-    console.log("Packaging windows!");
+    console.log("Packaging for windows.");
 
     util.copy(config.binPath, config.platformReleaseDir);
+
+    // We need to bring over MingW runtime dlls
+    console.log("Copying runtime DLLs");
+    const filesToBeMoved = fs.readdirSync(config.reveryBinPath).filter((f) => {
+      return path.extname(f) == ".dll";
+    });
+
+    filesToBeMoved.forEach((f) => {
+        util.copy(path.join(config.reveryBinPath, f), path.join(config.binPath));
+    });
 };
